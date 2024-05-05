72882fc9f0ae5692e65ccf9a471c8f433916c7a2
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-05-05 19:42:48 +0300

    Создана миграция 0001

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание категории')
+
+    def __str__(self):
+        return f'{self.name_category} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание продукта')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    create_at = models.DateField(verbose_name='дата создания')
+    update_at = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name_product} {self.description} {self.category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
