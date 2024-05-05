662bfd5ffcd78298e6a9029457d784b6a8f99e81
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-05-05 20:01:29 +0300

    Создана миграция 0002

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='цена за покупку')
     create_at = models.DateField(verbose_name='дата создания')
     update_at = models.DateField(verbose_name='дата последнего изменения')
+    manufactured_at = models.DateField(verbose_name='дата производства продукта', **NULLABLE)
 
     def __str__(self):
         return f'{self.name_product} {self.description} {self.category}'
